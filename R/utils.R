2732876052ca49bbe82fe93c1338952bf0cd1d7d
Fran <francesca.bryden@dft.gov.uk>
2023-10-03 10:31:04 +0100

    Tidy and document

--- R/utils.R
+++ R/utils.R
@@ -72,7 +72,7 @@ collect_links <- function(url){
 
 
 #' For a provided url, returns urls of uploaded ODS and CSV files associated with statistical publications
-#' @name collect_links
+#' @name scrape_tables
 #' @param url URL of the web page to scrape urls from
 scrape_tables <- function(url){
 
@@ -84,36 +84,5 @@ scrape_tables <- function(url){
   tibble(
     urls = links[grepl(".ods$|.csv$", links)])
 
-}
-
-#' For a provided url, return a table of urls of uploaded ODS and CSV files
-#' @name collect_links
-#' @param url URL of the web page to scrape urls from. Defaults to the gov.uk stats homepage
-#' @return a table containing three columns, the collection url, the document url and the time of collection
-#' @export
-
-all_table_urls <- function(url = "https://www.gov.uk/government/organisations/department-for-transport/about/statistics"){
-# put together full list of tables
-  purrr::map(.x = collect_collections(url),
-                         .f = collect_links) %>%
-  purrr::map_df(.f = scrape_tables,
-                .id = "collection") %>%
-  unique() %>%
-  ##Drop anything that starts with a hash
-  dplyr::filter(!grepl("^[#]", urls)) %>%
-  ##Create columns for table name and upload number
-  #Regex my fave
-  ##This will strip out everything before the final slash, regex are greedy by default
-  dplyr::mutate(file_name = gsub("^.*[/]", "", urls),
-                #Keep everything after the word file, then drop the file name
-                upload_id = gsub("^.*file[/]", "", urls),
-                upload_id = as.numeric(gsub("/.*", "", upload_id))) %>%
-  ##Group and slice to keep the highest upload no
-  dplyr::group_by(file_name) %>%
-  dplyr::arrange(desc(upload_no)) %>%
-  dplyr::slice(1L) %>%
-  ##Add process time
-  dplyr::mutate(time_of_check = Sys.time()) %>%
-  dplyr::rename(collection_url = collection, doc_url = urls)
 }
 
