570bb915db60c96b3dee4124bfdd79ad272ab320
Mohan Dell <mdell@gcp-rs-aviation.c.dft-ls-datacentre-prod.internal>
2024-08-09 15:30:53 +0100

    Added further checks for the year_column_check function

--- tests/testthat/test-year_column_check.R
+++ tests/testthat/test-year_column_check.R
@@ -6,7 +6,11 @@ data <- data.frame(year = c(2000, 2001, 2002, 2003, 2004, 2005),
                    Qtr5 = c("2000", "2001", "2002", "2003", "2004", "2005"),
                    Qtr6 = c(2000, 2001, 2002, 2003, 2004, 2005),
                    Qtr7 = c(2000, 2001, 2002, 2003, 2004, 2005),
-                   Qtr8 = c(2000, 2001, 2002, 2003, 2004, 2005)) %>% 
+                   Qtr8 = c(2000, 2001L, 2002, 2003, 2004, 2005),
+                   Qtr9 = c(2000, 2001L, 2002L, 2003, 2004, 2005),
+                   Qtr10 = c(2000, 2001, 0002, 2003, 2004, 2005),
+                   Qtr11 = c(""),
+                   Qtr12 = c("Cars", "Vans", "Buses", "Rail", "Ship", "Air")) %>% 
   transform(Qtr6 = as.integer(Qtr6),
             Qtr7 = as.numeric(Qtr7),
             Qtr8 = as.double(Qtr8))
@@ -17,10 +21,16 @@ testthat::test_that("checks on data work", {
   testthat::expect_true(year_column_check(data$Qtr6))
   testthat::expect_true(year_column_check(data$Qtr7))
   testthat::expect_true(year_column_check(data$Qtr8))
+  testthat::expect_true(year_column_check(data$Qtr9))
   
   testthat::expect_false(year_column_check(data$Qtr1))
   testthat::expect_false(year_column_check(data$Qtr4))
-  testthat::expect_false(year_column_check(data$Qtr2))
   testthat::expect_false(year_column_check(data$Qtr5))
-
+  testthat::expect_false(year_column_check(data$Qtr10))
+  testthat::expect_false(year_column_check(data$Qtr11))
+  testthat::expect_false(year_column_check(data$Qtr12))
+  
+  # result should be false, it is false, but test does not pass
+  testthat::expect_false(year_column_check(data$Qtr2))
+  
 })
