845f709f3769c558631a5b0efb57d57fb3f61b12
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-10-09 08:55:07 +0000

    Added bucket name as an argument

--- R/gcp_tables_to_list.R
+++ R/gcp_tables_to_list.R
@@ -1,16 +1,16 @@
 # function to download a published table, using the TS Finder list
-gcp_tables_to_list <- function(df_name){
+gcp_tables_to_list <- function(df_name, bucket_name = "tin_dev_data_storage"){
   f <- tempfile()
 
   # read in list of published tables, from the TS Finder project
-  full_table_list <- gcs_list_objects("tin_dev_data_storage") %>%
+  full_table_list <- gcs_list_objects(bucket_name) %>%
     dplyr::filter(grepl(df_name, name, ignore.case = TRUE)) %>%
     dplyr::arrange(desc(updated)) %>%
     dplyr::slice(1L) %>%
     dplyr::pull(name)
 
   # download the file using the URL
-  gcs_get_object(bucket = "tin_dev_data_storage",
+  gcs_get_object(bucket = bucket_name,
                  full_table_list,
                  overwrite = TRUE,
                  saveToDisk = f)
